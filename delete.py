cc9970cec0e5af88a935ad1ca229df93260dd52a
Amir <amir.hac@outlook.com>
2024-09-26 14:45:28 +0200

    delete updated

--- delete.py
+++ delete.py
@@ -3,9 +3,13 @@ import pymupdf
 
 filename = input("Filename: ")
 
+pgnum = input("Page number: ")
+
+pgnum = int(pgnum)
+
 doc = pymupdf.open(filename)
 
-doc.delete_page()
+doc.delete_page(pgnum)
 
 doc.save(f"del-{filename}")
 
