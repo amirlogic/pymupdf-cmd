c594f60beb3c2ea3c3886e10c99095de16b6df66
Amir <amir.hac@outlook.com>
2024-12-03 15:35:21 +0100

    Update file-htmlbox.py

--- file-htmlbox.py
+++ file-htmlbox.py
@@ -12,23 +12,15 @@ pgnum = int(input("Page: "))
 
 page = doc[pgnum]
 
-#textcontent = input("Text: ")
-
-""" rx0 = int(input("Rect x0: "))
-ry0 = int(input("Rect y0: "))
-rx1 = int(input("Rect x1: "))
-ry1 = int(input("Rect y1: ")) """
-
 newfile = input("New file: ")
 
 with open(src,'r') as srcfile:
 
-    page.insert_htmlbox(page.rect, srcfile.read())
+    res = page.insert_htmlbox(page.rect, srcfile.read())
 
-doc.save(newfile)
+    doc.save(newfile)
 
-
-print("Text inserted")
+    print("Text inserted",res)
 
 doc.close()
 
