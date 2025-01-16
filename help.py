51e41802ae937ff74e93c5510b43ac1883057cab
Amir <amir.hac@outlook.com>
2025-01-16 01:22:51 +0100

    some fixes

--- help.py
+++ help.py
@@ -1,8 +1,9 @@
 
+import sys
 
 hdata = [
 
-    [ "start.py", "Creates a one page pdf and inserts text in it" ],
+    [ "start.py", "Creates a one page pdf and inserts text in it*" ],
 
     [ "pixmap.py", "Saves page as png image" ],
 
@@ -16,13 +17,39 @@ hdata = [
 
     [ "search.py", "Find a word in a document" ],
 
+    [ "get-links.py", "Extract links from a document" ],
+
+    [ "text-link.py", "Turn text into link in a document" ],
+
+    [ "insert-link.py", "Inserts a link in a document (requires a Rect object)*" ],
+
+    [ "embfile.py", "Embed a file in a document*" ],
+
+    [ "embfile-multi.py", "Embed multiple files in a document" ],
+
     [ "bgcolor.py", "Edit background color for one page" ],
 
     [ "bgcolor-all.py", "Edit background color for all pages" ],
 
 ]
 
-print("\n\t","PyMuPDF Command Line Apps\n")
+print("\n\tPyMuPDF Command Line Apps\n")
 
-for line in hdata:
-    print("\t",line[0],"\t",line[1],"\n")
+if(len(sys.argv)>1):
+    if(sys.argv[1] == "start"):
+        print("\tCreates a new PDF file\n")
+
+    elif(sys.argv[1] == "insert-link"):
+        print("\tinsert-link\n")
+        print("\tSupported links: LINK_GOTO (1) and LINK_URI (2)\n")
+
+    elif(sys.argv[1] == "embfile"):
+        print("\tembfile - Embed a file\n")
+        print("\tembfile \n")
+    else:
+        print("\tUnknown script\n")
+        
+else:
+
+    for line in hdata:
+        print("\t",line[0],"\t",line[1],"\n")
