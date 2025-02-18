554dec370522ce5cb8a4ff6f0b6e4b91fccb50bf
Amir <amir.hac@outlook.com>
2025-02-18 16:33:19 +0100

    Update toc.py

--- toc.py
+++ toc.py
@@ -1,17 +1,15 @@
 import pymupdf
 
+import pprint
+
 print("Get Table of Content")
 
 filename = input("Filename: ")
 
 doc = pymupdf.open(filename)
 
-#pgno = int(input("Page: "))
+#print("Table of Content")
 
-#doc.save(filename[:-4]+"_compressed.pdf",garbage=4,deflate=True)
-
-#page = doc[p]
-
-print(doc.get_toc())
+pprint.pp(doc.get_toc())
 
 print("Task complete")
