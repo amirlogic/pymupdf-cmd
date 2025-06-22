272ddfcc9561f2236ffebeec50a1ecea9641cf56
Amir <amir.hac@outlook.com>
2025-06-22 15:46:54 +0200

    affected pages

--- trim-page.py
+++ trim-page.py
@@ -6,6 +6,8 @@ src = pymupdf.open(filename)
 
 out = pymupdf.open()
 
+print("Affected pages (zero-based):")
+
 start = int(input("Start page (default 0): "))
 
 end = int(input("End page (default last page): "))
@@ -22,9 +24,14 @@ for p in range(0,len(src)):
     out.new_page()
     page = out[pgnum]
 
-    crop = pymupdf.Rect( 0, page.rect.y0+top, page.rect.x1, page.rect.y1-bottom )
+    if(p >= start and p <= end):
 
-    page.show_pdf_page(page.rect,src,pgnum,clip=crop)
+        crop = pymupdf.Rect( 0, page.rect.y0+top, page.rect.x1, page.rect.y1-bottom )
+
+        page.show_pdf_page(page.rect,src,pgnum,clip=crop)
+
+    else:
+        page.show_pdf_page(page.rect,src,pgnum)
 
     pgnum += 1
 
