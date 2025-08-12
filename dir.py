f5c0578f9b6b9085fd797c40cd9fba41c3192855
Amir <amir.hac@outlook.com>
2025-08-12 19:32:29 +0200

    directory analyzer

---
+++ dir.py
@@ -0,0 +1,50 @@
+
+import pymupdf
+
+import pathlib
+
+print("\nAnalyze PDF Documents in a Directory\n")
+
+wdir = input("Directory: ")
+
+wlkpath = pathlib.Path(wdir)
+
+for file_path in wlkpath.iterdir():
+
+    if(file_path.suffix == ".pdf"):
+
+        print(file_path.name)
+
+        try:
+
+            doc = pymupdf.open(file_path)
+
+            toclen = len(doc.get_toc())
+
+            print("pages:",doc.page_count,"- TOC items:",toclen,"- Embedded files:",doc.embfile_count())
+
+            if(doc.is_form_pdf != False):
+                print("This PDF has forms")
+
+            #print(doc.get_layer())
+
+            lnkcnt = 0
+
+            imgcnt = 0
+
+            for page in doc:
+
+                lnkcnt += len(page.get_links())
+
+                imgcnt += len(page.get_images())
+
+                
+
+            print("Links:",lnkcnt,"- Images:",imgcnt)
+
+            print("\n")
+        
+        except:
+            print("Error")
+
+        
