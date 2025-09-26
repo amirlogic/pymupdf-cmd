d383be29e37c860ec0669f2c775deca8bd56cf88
Amir <amir.hac@outlook.com>
2025-09-26 16:01:50 +0200

    OCR checker font added

--- textvsimg.py
+++ textvsimg.py
@@ -10,6 +10,8 @@ txtcount = 0
 
 imgcount = 0
 
+docfonts = []
+
 for page in doc:
 
     r = page.get_text("dict")
@@ -24,10 +26,24 @@ for page in doc:
 
             imgcount += 1
 
+    pagefonts = page.get_fonts()
+
+    for f in pagefonts:
+
+        if(f[3] not in tuple(docfonts)):
+
+            docfonts.append(f[3])
+
+
 print("Text blocks:",txtcount)
 
 print("Image count:",imgcount)
 
+print(f"Fonts ({len(docfonts)})")
+
+for ft in docfonts:
+
+    print(ft)
 
 doc.close()
 
