e4c17799c6f1f04f4a19133ae4fdf58f9c432ee8
Amir <amir.hac@outlook.com>
2025-12-09 17:49:13 +0100

    shapes line-rect-circle

---
+++ shape.py
@@ -0,0 +1,66 @@
+
+import pymupdf
+
+doc = pymupdf.open()
+
+doc.new_page()
+
+print("Draw a Shape\n")
+
+print("1. Line")
+print("2. Rectangle")
+print("3. Circle")
+
+snum = input("Shape #: ")
+
+page = doc[0]
+
+
+if(snum == "1"):
+
+    shape = page.new_shape()
+
+    pt1 = input("Point 1 (x,y): ").split(",")
+
+    pt2 = input("Point 2 (x,y): ").split(",")
+
+    page.draw_line(pymupdf.Point(int(pt1[0]),int(pt1[1])),pymupdf.Point(int(pt2[0]),int(pt2[1])))
+
+    shape.commit()
+
+
+elif(snum == "2"):
+     
+    shape = page.new_shape()
+
+    rc = input("Rectangle: (x0,y0,x1,y1): ")
+
+    rect = rc.split(",")
+
+    page.draw_rect(pymupdf.Rect(*rect))
+
+    shape.commit()
+
+
+elif(snum == "3"):
+     
+    shape = page.new_shape()
+
+    rd = input("Center: (x,y)").split(",")
+    
+    radius = input("Radius: ")
+
+    page.draw_circle(pymupdf.Point(*rd))
+
+    shape.commit()
+
+else:
+
+    print("Not supported")
+
+
+exported = input("Exported: ")
+
+doc.ez_save(exported)
+
+doc.close()
